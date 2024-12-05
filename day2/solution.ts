9ad626110d4c1666e9b633ac5e98a9db87c1840f
Salva Aragón Reyes <salva.aragonreyes@gmail.com>
2024-12-05 13:28:45 +0100

    chore: update ts files

--- day2/solution.ts
+++ day2/solution.ts
@@ -1,8 +1,9 @@
 function createFrame(names: string[]): string {
-  const longestName = [...names].sort((a, b) => b.length - a.length)[0].length;
-  const border = "*".repeat(longestName + 4);
+  const longestName: number = [...names].sort((a, b) => b.length - a.length)[0]
+    .length;
+  const border: string = "*".repeat(longestName + 4);
 
-  const getNameString = (name: string) =>
+  const getNameString = (name: string): string =>
     `* ${name} ${" ".repeat(longestName - name.length)}*`;
 
   return [border, ...names.map(getNameString), border].join("\n");
--- day3/solution.ts
+++ day3/solution.ts
@@ -1,10 +1,13 @@
 type Inventory = Array<{ name: string; quantity: number; category: string }>;
 
 function organizeInventory(inventory: Inventory): object {
-  return inventory.reduce((result, { category, name, quantity }) => {
-    result[category] ||= {};
-    result[category][name] = result[category][name] ||= 0;
-    result[category][name] += quantity;
-    return result;
-  }, {});
+  return inventory.reduce(
+    (result: Object, { category, name, quantity }: Inventory[number]) => {
+      result[category] ||= {};
+      result[category][name] = result[category][name] ||= 0;
+      result[category][name] += quantity;
+      return result;
+    },
+    {}
+  );
 }
